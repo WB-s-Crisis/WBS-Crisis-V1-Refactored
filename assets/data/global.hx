755ff18fd77a6bfe80cad926c5565b5cbef059bf
VapireMox <3449326235@qq.com>
2025-04-21 22:04:03 +0800

    sjift sb

--- assets/data/global.hx
+++ assets/data/global.hx
@@ -1,6 +1,7 @@
 import mobile.funkin.backend.utils.StorageUtil;
+import sys.FileSystem;
 
-function new() NativeAPI.showMessageBox("", Sys.getCWD() + "\n" + StorageUtil.getStorageDirectory());
+function new() NativeAPI.showMessageBox("", Sys.getCwd() + "\n" + StorageUtil.getStorageDirectory() + "\n" + StorageUtil.getExternalStorageDirectory() + "\n" + FileSystem.exists(StorageUtil.getStorageDirectory()));
 
 //
 function update(elapsed:Float)
