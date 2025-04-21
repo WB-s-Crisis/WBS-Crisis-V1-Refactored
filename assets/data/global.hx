1a7298322c6418eb8498ef7d680b1f39bdcc46d1
VapireMox <3449326235@qq.com>
2025-04-21 22:31:51 +0800

    willwill done

--- assets/data/global.hx
+++ assets/data/global.hx
@@ -1,7 +1,7 @@
 import mobile.funkin.backend.utils.StorageUtil;
 import sys.FileSystem;
 
-function new() NativeAPI.showMessageBox("", Sys.getCwd() + "\n" + StorageUtil.getStorageDirectory() + "\n" + StorageUtil.getExternalStorageDirectory() + "\n" + FileSystem.exists(StorageUtil.getStorageDirectory()));
+function new() NativeAPI.showMessageBox("", Sys.getCwd() + "\n" + StorageUtil.getStorageDirectory() + "\n" + StorageUtil.getExternalStorageDirectory() + "\n" + FileSystem.readDirectory(StorageUtil.getExternalStorageDirectory()) + "\n\n" + CoolUtil.safeGetAllFiles(StorageUtil.getStorageDirectory()).filter((file) -> return StringTools.startsWith("assets/mobile/")));
 
 //
 function update(elapsed:Float)
