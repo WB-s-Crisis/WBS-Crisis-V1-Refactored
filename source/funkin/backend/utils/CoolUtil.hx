560a9b77caac64989fbbb0cd11747cad0aeceb41
GeXie <3449326235@qq.com>
2025-04-22 16:37:21 +0800

    s

--- source/funkin/backend/utils/CoolUtil.hx
+++ source/funkin/backend/utils/CoolUtil.hx
@@ -84,6 +84,53 @@ class CoolUtil
 	}
 
 	/**
+	 * Safely retrieve all files in the specified directory and subdirectories.
+	 * @param originPath Specify directory
+	 * @param reservePath Add the path of the specified directory to the output file or not.
+	 * @param stayEmptyDirectory Keep an empty directory or not.
+	 * @param showErrorBox When an error is captured, should alert be displayed.
+	 */
+	@:noUsing public inline static function safeGetAllFiles(originPath:String, reservePath:Bool = false, stayEmptyDirectory = true, showErrorBox:Bool = false):Array<String> {
+		#if sys
+		var files:Array<String> = [];
+		originPath = Path.addTrailingSlash(originPath);
+
+		var errorMessages:Array<String> = [];
+		function traverse(currentPath:String) {
+			try {
+				if (FileSystem.exists(currentPath) && FileSystem.isDirectory(currentPath)) {
+					final sb = FileSystem.readDirectory(currentPath);
+					if(sb.length > 0) for (item in sb) {
+						var fullPath = Path.addTrailingSlash(currentPath) + item;
+						if (FileSystem.isDirectory(fullPath)) {
+							traverse(fullPath);
+						} else {
+							files.push(reservePath ? fullPath : fullPath.substr(originPath.length));
+						}
+					}
+					else if(stayEmptyDirectory) {
+						files.push(reservePath ? currentPath : currentPath.substr(originPath.length));
+					}
+				}
+			} catch(e:haxe.Exception) {
+				errorMessages.push('Reading This Directory "$currentPath" Failed! \n[${e.message}\n${Std.string(e.stack)}]');
+			}
+		}
+
+		traverse(originPath);
+		if(errorMessages.length > 0) {
+			for(error in errorMessages) {
+				if(showErrorBox) funkin.backend.utils.NativeAPI.showMessageBox("Get All Files Error!", error);
+				Logs.trace(error, ERROR);
+			}
+			return [];
+		}
+		return files;
+		#end
+		return [];
+	}
+
+	/**
 	 * Safe saves a file (even adding eventual missing folders) and shows a warning box instead of making the program crash
 	 * @param path Path to save the file at.
 	 * @param content Content of the file to save (as String or Bytes).
@@ -783,45 +830,6 @@ class CoolUtil
 		var file = new haxe.io.Path(file);
 		return file.file;
 	}
-
-	#if sys
-	@:noUsing public static inline function safeGetAllFiles(originPath:String, reservePath:Bool = false, stayEmptyDirectory = true, showErrorBox:Bool = #if mobile true #else false #end):Array<String> {
-		var files:Array<String> = [];
-		originPath = Path.addTrailingSlash(originPath);
-
-		var errorMessages:Array<String> = [];
-		function traverse(currentPath:String) {
-			try {
-				if (FileSystem.exists(currentPath) && FileSystem.isDirectory(currentPath)) {
-					final sb = FileSystem.readDirectory(currentPath);
-					if(sb.length > 0) for (item in sb) {
-						var fullPath = Path.addTrailingSlash(currentPath) + item;
-						if (FileSystem.isDirectory(fullPath)) {
-							traverse(fullPath);
-						} else {
-							files.push(reservePath ? fullPath : fullPath.substr(originPath.length));
-						}
-					}
-					else if(stayEmptyDirectory) {
-						files.push(reservePath ? currentPath : currentPath.substr(originPath.length));
-					}
-				}
-			} catch(e:haxe.Exception) {
-				errorMessages.push('Reading This Directory "$currentPath" Failed! \n (${e.message}) \n (${Std.string(e.stack)})');
-			}
-		}
-
-		traverse(originPath);
-		if(errorMessages.length > 0) {
-			for(error in errorMessages) {
-				if(showErrorBox) lime.app.Application.current.window.alert(error, "Get All Files Error!");
-				Logs.trace("Get All Files Error: " + error, ERROR);
-			}
-			return [];
-		}
-		return files;
-	}
-	#end
 
 	/**
 	 * Converts a string of "1..3,5,7..9,8..5" into an array of numbers like [1,2,3,5,7,8,9,8,7,6,5]
